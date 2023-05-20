b514c8a9b1a48341c7c33ab6398bc32067992b1c
RussellAbraham <russell_a@live.com>
2023-05-20 08:49:58 -0700

    Added eqArrays function to compare 1 dimensional array equality

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  try {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      throw new Error(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    console.log('Assertion completed.');
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]){
+      return false
+    }
+  }
+  return true;
+};
