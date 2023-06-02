1d72f74bc78a7c47d810bfbfa877a3929066cfb9
RussellAbraham <russell_a@live.com>
2023-06-02 14:01:08 -0700

    added findKey to lotide library

---
+++ findKey.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  try {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      throw new Error(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    console.log('Assertion completed.');
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+
+// Example data
+const data = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+// Test case 1: Callback returns true for "noma"
+
+const result1 = findKey(data, x => x.stars === 2);
+assertEqual(result1, "noma");
+
+// Test case 2: Callback returns true for "Akaleri"
+const result2 = findKey(data, x => x.stars > 2);
+assertEqual(result2, "Akaleri");
+
+// Test case 3: Callback returns true for "Ora"
+const result3 = findKey(data, x => x.stars === 2 && x.name === "Ora");
+assertEqual(result3, undefined);
