a36f55cc16df903a5a707195ee7079f18753c56f
RussellAbraham <russell_a@live.com>
2023-06-09 19:13:26 -0700

    Refactored index.js to use object shorthand

--- index.js
+++ index.js
@@ -10,31 +10,31 @@ const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
-const head   = require('./head');
+const head = require('./head');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
 const middle = require('./middle');
-const tail   = require('./tail');
+const tail = require('./tail');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
-  assertArraysEqual : assertArraysEqual,
-  assertEqual : assertEqual,
-  assertObjectsEqual : assertObjectsEqual,
-  countLetters : countLetters,
-  countOnly : countOnly,
-  each : each,
-  eqArrays : eqArrays,
-  eqObjects : eqObjects,
-  findKey : findKey,
-  findKeyByValue : findKeyByValue,
-  flatten : flatten,
-  head:   head,
-  letterPositions : letterPositions,
-  map : map,
-  middle: middle,
-  tail:   tail,
-  takeUntil : takeUntil,
-  without : without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  each,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
