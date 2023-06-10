d909f9daa99606be337f7a928ca50ab3f0ca7ea2
RussellAbraham <russell_a@live.com>
2023-06-09 18:40:28 -0700

    Refactored test/headTest to use moch and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,15 +1,50 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1, 2, 3]), 1); // Expected: 1, Actual: 1 (First element is 1)
-assertEqual(head(['a', 'b', 'c']), 'a'); // Expected: 'a', Actual: 'a' (First element is 'a')
-assertEqual(head([]), undefined); // Expected: undefined, Actual: undefined (Empty array)
-assertEqual(head([5]), 5); // Expected: 5, Actual: 5 (Single-element array)
-assertEqual(head(['apple', 'banana', 'cherry']), 'apple'); // Expected: 'apple', Actual: 'apple' (First element is 'apple')
-assertEqual(head([null, undefined, true]), null); // Expected: null, Actual: null (First element is null)
-assertEqual(head([false, true]), false); // Expected: false, Actual: false (First element is false)
-assertEqual(head(['hello', 'world']), 'hello'); // Expected: 'hello', Actual: 'hello' (First element is 'hello')
-assertEqual(head([0, 1, 2]), 0); // Expected: 0, Actual: 0 (First element is 0)
-assertEqual(head([NaN, Infinity]), NaN); // Expected: NaN, Actual: NaN (First element is NaN)
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 'a' for ['a', 'b', 'c']", () => {
+    assert.strictEqual(head(['a', 'b', 'c']), 'a');
+  });
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+  it("returns 'apple' for ['apple', 'banana', 'cherry']", () => {
+    assert.strictEqual(head(['apple', 'banana', 'cherry']), 'apple');
+  });
+  it("returns null for [null, undefined, true]", () => {
+    assert.strictEqual(head([null, undefined, true]), null);
+  });
+  it("returns false for [false, true]", () => {
+    assert.strictEqual(head([false, true]), false);
+  });
+  it("returns 'hello' for ['hello', 'world']", () => {
+    assert.strictEqual(head(['hello', 'world']), 'hello');
+  });
+  it("returns 0 for [0, 1, 2]", () => {
+    assert.strictEqual(head([0, 1, 2]), 0);
+  });
+  it("returns NaN for [NaN, Infinity]", () => {
+    assert.strictEqual(head([NaN, Infinity]), NaN);
+  });
+});
