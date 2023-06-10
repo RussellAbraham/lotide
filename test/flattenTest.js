eea712c25d1085ba3ec776eb51f380ba3ea79734
RussellAbraham <russell_a@live.com>
2023-06-09 17:43:20 -0700

    Incomplete update to flatten test

---
+++ test/flattenTest.js
@@ -0,0 +1,33 @@
+// functions for testing flatten - LHL directive
+
+const assertEqual = function(actual, expected) {
+  try {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      throw new Error(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    console.log('Assertion completed.');
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// demo
+const test = [1,2,[3,4],5,[6]];
+const runTest = flatten(test);
+assertEqual(eqArrays(runTest, [1, 2, 3, 4, 5, 6]), true);
